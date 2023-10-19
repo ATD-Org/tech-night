6466e4717883cb77215acafcdf9f944639efed7d
Alain Tran-Dang <alain.tran-dang@avanade.com>
2023-10-19 16:32:37 -0400

    fix app code to pass unit test

--- My-WebAPI/WebAPI/Controllers/WeatherForecastController.cs
+++ My-WebAPI/WebAPI/Controllers/WeatherForecastController.cs
@@ -26,7 +26,7 @@ namespace WebAPI.Controllers
         [HttpGet(Name = "GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
         {
-            return Enumerable.Range(1, 6).Select(index => new WeatherForecast
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateTime.Now.AddDays(index),
                 TemperatureC = Random.Shared.Next(-20, 55),
